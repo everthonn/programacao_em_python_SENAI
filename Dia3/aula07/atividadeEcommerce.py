ebefc0bc05a19248f3fd362c95a32dc2488eb5df
Everthon <everthondier4@gmail.com>
2025-03-30 00:08:39 -0300

    finalizado ecommerce

--- Dia3/aula07/atividadeEcommerce.py
+++ Dia3/aula07/atividadeEcommerce.py
@@ -1,9 +1,9 @@
 ecommerce = {
 
     "fones" : ["jbl","qcy","samsung"],
-    "valoresFones" : [10,15,25],
+    "valoresFones" : [10.0,15.0,25.0],
     "computadores" : ["mac","dell","lg"],
-    "valoresComputadores" : [10,15,25]
+    "valoresComputadores" : [10.0,15.0,25.0]
 }
 
 # 1 comprar 
@@ -18,15 +18,18 @@ carrinho.append([ecommerce[escolha_tipo2][int(input("0,1,2"))]])
 
 valorItem = input("digite valores e o item desejado ex: valoresFones")
 valor = int(input("0,1,2"))
-valores.append(([ecommerce[valorItem][int(valor)]]))
+valores += (([ecommerce[valorItem][int(valor)]]))
 valorItem2 = input("digite valores e o item desejado ex: valoresFones")
 valor2 = int(input("0,1,2"))
-valores.append([ecommerce[valorItem2][int(valor2)]])
+valores += ([ecommerce[valorItem2][int(valor2)]])
 
-soma=sum(valores)
-print(soma)
-print(valores)
+soma = sum(valores)
+print("os itens selecionados serão exibidos abaixo juntamente com seus respectivos valores")
 print(carrinho)
+print(valores)
+
+print("Valor total de R$ ",soma)
+
 # itens = sum(carrinho)
 
 
