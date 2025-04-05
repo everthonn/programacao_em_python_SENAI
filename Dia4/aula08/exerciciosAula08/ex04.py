27be84caccd0e4dad00e23bc6554f772b5916c88
everthon Luiz <everthondier4@gmail.com>
2025-04-05 15:05:18 -0300

    reserva de hotel

--- Dia4/aula08/exerciciosAula08/ex04.py
+++ Dia4/aula08/exerciciosAula08/ex04.py
@@ -3,7 +3,7 @@ lado2 = int(input("digite o lado do triangulo"))
 lado3 = int(input("digite o lado do triangulo"))
 
 if lado1 >0 and lado2 > 0 and lado3 > 0 :
-    if lado1 == lado2 and lado3 == lado1:
+    if lado1 == lado2 and lado3 == lado2:
         print("triangulo equilatero")
     elif (lado2 == lado3 and lado2 != lado1) or (lado1 == lado3 and lado1 != lado2) or (lado1 == lado2 and lado1 != lado3) :
         print("triangulo isosceles")
---
+++ Dia4/aula08/reservasHotel.py
@@ -0,0 +1,86 @@
+inicio = int(input("quantos clientes deseja cadastrar?"))
+
+if inicio == 3:
+
+    hotel = {
+        "simples":100,
+        "duplo":150,
+        "luxo":250
+    }
+
+    nomeCliente1 = input("nome do cliente 1: ")
+    idadeCliente1 = input("idade cliente 1: ")
+    diariasCliente1 = float(input("quantos dias deseja permanecer"))
+    escolhaSuite1 = input("qual o quarto desejado")
+    
+
+    #////////////////////////////////////////////////////
+
+    nomeCliente2 = input("nome do cliente 2: ")
+    idadeCliente2 = input("idade cliente 2: ")
+    diariasCliente2 = float(input("quantos dias deseja permanecer"))
+    escolhaSuite2 = input("qual o quarto desejado")
+    
+
+    #//////////////////////////////////////////
+
+    nomeCliente3 = input("nome do cliente 3: ")
+    idadeCliente3 = input("idade cliente 3: ")
+    diariasCliente3 = float(input("quantos dias deseja permanecer"))
+    escolhaSuite3 = input("qual o quarto desejado")
+
+
+
+    valor1 = hotel[escolhaSuite1]*diariasCliente1
+    valor2 = hotel[escolhaSuite2]*diariasCliente2
+    valor3 = hotel[escolhaSuite3]*diariasCliente3
+
+
+    print("nome:",nomeCliente1,"   suite:",escolhaSuite1,"   diarias:",diariasCliente1,"\n","nome:", nomeCliente2,"   suite:",escolhaSuite2,"   diarias:",diariasCliente2,"\n","nome:",nomeCliente3,"   suite:",escolhaSuite3,"   diarias:",diariasCliente3)
+    print("valores \n cliente 1",valor1,"\n cliente 2",valor2,"\n cliente 3",valor3)
+
+elif inicio == 2:
+    hotel = {
+        "simples":100,
+        "duplo":150,
+        "luxo":250
+    }
+
+    nomeCliente1 = input("nome do cliente 1: ")
+    idadeCliente1 = input("idade cliente 1: ")
+    diariasCliente1 = float(input("quantos dias deseja permanecer"))
+    escolhaSuite1 = input("qual o quarto desejado")
+    
+
+    #////////////////////////////////////////////////////
+
+    nomeCliente2 = input("nome do cliente 2: ")
+    idadeCliente2 = input("idade cliente 2: ")
+    diariasCliente2 = float(input("quantos dias deseja permanecer"))
+    escolhaSuite2 = input("qual o quarto desejado")
+
+    valor1 = hotel[escolhaSuite1]*diariasCliente1
+    valor2 = hotel[escolhaSuite2]*diariasCliente2
+
+    print("nome:",nomeCliente1,"   suite:",escolhaSuite1,"   diarias:",diariasCliente1,"\n","nome:", nomeCliente2,"   suite:",escolhaSuite2,"   diarias:",diariasCliente2)
+    print("valores \n cliente 1",valor1,"\n cliente 2",valor2)
+
+elif inicio == 1:
+
+    
+    hotel = {
+        "simples":100,
+        "duplo":150,
+        "luxo":250
+    }
+
+    nomeCliente1 = input("nome do cliente 1: ")
+    idadeCliente1 = input("idade cliente 1: ")
+    diariasCliente1 = float(input("quantos dias deseja permanecer"))
+    escolhaSuite1 = input("qual o quarto desejado")
+
+    valor1 = hotel[escolhaSuite1]*diariasCliente1
+    print("nome:",nomeCliente1,"   suite:",escolhaSuite1,"   diarias:",diariasCliente1)
+    print("valor \n cliente 1",valor1)
+else:
+    print("impossivel cadastrar mais que 3 pessoas")
